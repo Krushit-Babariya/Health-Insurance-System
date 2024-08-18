f21cb2021e00a8df6249deecdea9de3a21aed290
Krushit-Babariya <krushitsbabariya41@gmial.com>
2024-08-18 09:29:27 +0530

    Data Collection Module Added

---
+++ HIS-DataCollectionService/src/main/java/com/krushit/HisDataCollectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krushit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisDataCollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisDataCollectionServiceApplication.class, args);
+	}
+
+}
