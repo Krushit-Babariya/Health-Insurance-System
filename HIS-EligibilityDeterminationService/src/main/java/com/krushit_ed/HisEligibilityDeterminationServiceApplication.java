c670a3a1e1f1a011d60542274518b71630d29bce
Krushit-Babariya <krushitsbabariya41@gmial.com>
2024-08-23 22:01:05 +0530

    Eligibility Determination Module Added

---
+++ HIS-EligibilityDeterminationService/src/main/java/com/krushit_ed/HisEligibilityDeterminationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krushit_ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisEligibilityDeterminationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisEligibilityDeterminationServiceApplication.class, args);
+	}
+
+}
---
+++ HIS-EligibilityDeterminationService/src/test/java/com/krushit_ed/HisEligibilityDeterminationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krushit_ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisEligibilityDeterminationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
